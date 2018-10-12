29068c3f9167367c188c9d32e6df70fb4403d19a
Daniel Kantor <git@daniel-kantor.com>
2018-10-12 23:43:42 +0200

    perf: make emuto-loader cacheable

--- src/loader.js
+++ src/loader.js
@@ -1,7 +1,9 @@
 import compiler from 'emuto/lib/compiler';
 
-export default source =>
-  `
+export default source => {
+  if (this.cacheable) this.cacheable();
+  return `
     import builtins from 'emuto/lib/builtins';
     export default (${compiler(source)})(builtins);
   `.trim();
+};
